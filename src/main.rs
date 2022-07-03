f31b16dfcad38dc1621a4d4b9692d52aaf419db5
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-03 12:04:26 +1000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::process::Command;
+
+static OUTPUT_DIR: &'static str = "/home/tom/Desktop/tmp";
+static REPO_URL: &'static str =
+    "https://github.com/tberghuis/watch-and-read-comments-for-youtube.git";
+
 fn main() {
-    println!("Hello, world!");
+    Command::new("cd").arg(OUTPUT_DIR).spawn();
 
-
-    
+    Command::new("git").arg("clone").arg(REPO_URL).spawn();
 }
