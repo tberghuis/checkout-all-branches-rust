17ee2c132d7e0824f72552e8c4c00a65cf87a2dc
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-03 13:40:15 +1000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::process::Command;
+use std::io;
+use std::io::{BufRead, BufReader, Write};
+use std::process::{Command, Stdio};
 
 static OUTPUT_DIR: &'static str = "/home/tom/Desktop/tmp";
 static REPO_URL: &'static str =
-    "https://github.com/tberghuis/watch-and-read-comments-for-youtube.git";
+  "https://github.com/tberghuis/watch-and-read-comments-for-youtube.git";
 
-fn main() {
-    Command::new("cd").arg(OUTPUT_DIR).spawn();
+fn main() -> io::Result<()> {
+  let mut child_shell = Command::new("/bin/bash")
+    .stdin(Stdio::piped())
+    // .stdout(Stdio::piped())
+    .spawn()
+    .unwrap();
 
-    Command::new("git").arg("clone").arg(REPO_URL).spawn();
+  let child_in = child_shell.stdin.as_mut().unwrap();
+
+
+  let cmd = format!("cd {}\n", OUTPUT_DIR);
+  child_in.write_all(cmd.as_bytes())?;
+  let cmd = format!("git clone {}\n", REPO_URL);
+  child_in.write_all(cmd.as_bytes())?;
+
+  child_shell.wait_with_output();
+  Ok(())
 }
